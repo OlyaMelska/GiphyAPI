8113671700d8993de8c35a81bdff64d75e15583e
candye_sweet <olyamelska@Olhas-MBP.home>
2019-02-20 22:21:41 -0500

    Adding some functions

--- Assets/JavaScript/java.js
+++ Assets/JavaScript/java.js
@@ -4,7 +4,8 @@ let actors = [
   "Jennifer Anniston",
   "Bred Pitt",
   "Mila Kunis",
-  "Johnny Depp"
+  "Johnny Depp",
+  "Bradley Cooper"
 ];
 let displayBtn = $("#buttons");
 
@@ -12,7 +13,9 @@ let displayBtn = $("#buttons");
 function renderButtons() {
   $("#buttons").empty();
   for (let i = 0; i < actors.length; i++) {
-    let actorBtn = $("<button>").attr("class", "btn btn-dark");
+    let actorBtn = $("<button>")
+      .attr("class", "btn btn-dark")
+      .attr("actor", actors[i]);
     actorBtn.text(actors[i]);
     displayBtn.append(actorBtn);
   }
@@ -23,12 +26,9 @@ $("#submit").on("click", function() {
   event.preventDefault();
   let newActor;
   newActor = $("#actor-input").val();
-  console.log(newActor);
   actors.push(newActor);
-  console.log(newActor);
   renderButtons();
 });
-
 renderButtons();
 
 $("button").on("click", function() {
@@ -44,44 +44,25 @@ $("button").on("click", function() {
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-    console.log(response);
     let results = response.data;
     for (let i = 0; i < results.length; i++) {
-      let imgURL = results[i].images.fixed_height.url;
-      let imgRating = $("<p>").text("Rating: " + results[i].rating);
+      let imgURL = results[i].images.fixed_width_still.url;
+      let imgRating = $("<p>")
+        .text("Rating: " + results[i].rating)
+        .attr("class", "card-title");
       let imgElement = $("<img>");
-      let imgDiv = $("<div>");
+      let cardDiv = $("<div>").attr("class", "card");
 
-      imgElement.attr("src", imgURL);
-      imgDiv.append(imgElement);
-      imgDiv.append(imgRating);
-      $("#gifs-appear-here").append(imgElement, imgRating);
+      imgElement.attr("src", imgURL).attr("class", "card-img-top");
+      cardDiv.append(imgElement, imgRating);
+      $("#result").append(cardDiv);
+      $("img").on("click", event => {
+        let newImgURL = results[i].images.fixed_height.url;
+        console.log("new url " + newImgURL);
+        let clickedImg = $(event.target);
+        console.log("clicked img " + clickedImg);
+        clickedImg.attr("src", newImgURL);
+      });
     }
   });
 });
-
-// Calling the renderButtons function to display the initial list of movies
-
-// let movieOutput = document.getElementById("movie-view");
-// $("#find-movie").on("click", function(event) {
-//   event.preventDefault();
-
-//   let movie = $("#movie-input").val();
-//   let queryURL =
-//     "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
-//   console.log(queryURL);
-//   $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//   }).then(function(response) {
-//     let title = document.createElement("h1");
-//     let img = document.createElement("img");
-//     let plot = document.createElement("p");
-//     title.innerHTML = response.Title;
-//     console.log(img);
-//     img.src = response.Poster;
-//     plot.innerHTML = response.Plot;
-//     movieOutput.append(title, img, plot);
-//     console.log(response);
-//   });
-// });
